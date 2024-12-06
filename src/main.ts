8d3cdda21cf3a4b283e9193a8528f516b7ba37d3
cher1shRXD <taewoo080401@gmail.com>
2024-12-06 14:50:55 +0900

    feat: build setting

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import "./style.css";
-import App from "./App.vue";
 import router from "./router";
 import { useToast } from "./utils/toast/useToast";
 import store from "./store";
+import App from "./App.vue";
 
 const app = createApp(App);
 
